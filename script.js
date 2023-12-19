21a528c8e76cfc99c5689c349035539382e6a1fb
Nuri <infonuriasinacento@gmail.com>
2023-12-19 09:03:58 +0100

    added code from Jaume to create dynamic pages + started fix on carruousel btn

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function toggleMenu() {
   var overlay = document.getElementById('overlay');
+  var btncarrusel = document.getElementById('navigation-manual');
+
   overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
 
   var menu = document.querySelector('.menu');
@@ -7,4 +9,18 @@ function toggleMenu() {
   
   var body = document.querySelector('body');
   body.style.overflow = body.style.overflow === 'hidden' ? 'auto' : 'hidden';
+
+  // Verificar si el overlay está visible
+  if (overlay.style.display === 'block') {
+    // Si está visible, ocultar el botón
+    btncarrusel.style.display = 'none';
+  } else {
+    // Si no está visible, mostrar el botón
+    btncarrusel.style.display = 'block';
+  }
 }
+  
+
+    
+
+
