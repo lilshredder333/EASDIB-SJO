378efaee502a6a88ed86d245b833232eeecac289
Nuri <infonuriasinacento@gmail.com>
2023-12-19 16:32:59 +0100

    fixed issues btn and dropdown

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function toggleMenu() {
   var overlay = document.getElementById('overlay');
-  var btncarrusel = document.getElementById('navigation-manual');
-
-  // Restablecer estilos de los botones del carrusel
-  
 
   overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
 
@@ -13,15 +9,6 @@ function toggleMenu() {
   var body = document.querySelector('body');
   body.style.overflow = body.style.overflow === 'hidden' ? 'auto' : 'hidden';
 
-  // Verificar si el overlay está visible después de restablecer los estilos
-  if (overlay.style.display === 'block') {
-      // Si está visible, ocultar el botón
-      btncarrusel.style.display = 'none';
-  } else {
-      // Si no está visible, mostrar el botón
-      btncarrusel.style.display = 'block';
-      btncarrusel.style.transition = 'none'; // Deshabilitar temporalmente las transiciones    
-  }
 }
 
 
