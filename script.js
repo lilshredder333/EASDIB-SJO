6b0b81797f2c199f51a25bf16fc57699a9d2a6f1
Nuri <infonuriasinacento@gmail.com>
2023-12-19 09:35:36 +0100

    carrousel btn centered, still needs fix on size

--- script.js
+++ script.js
@@ -2,23 +2,29 @@ function toggleMenu() {
   var overlay = document.getElementById('overlay');
   var btncarrusel = document.getElementById('navigation-manual');
 
+  // Restablecer estilos de los botones del carrusel
+  
+
   overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block';
 
   var menu = document.querySelector('.menu');
   menu.style.right = menu.style.right === '0px' ? '-250px' : '0px';
-  
+
   var body = document.querySelector('body');
   body.style.overflow = body.style.overflow === 'hidden' ? 'auto' : 'hidden';
 
-  // Verificar si el overlay está visible
+  // Verificar si el overlay está visible después de restablecer los estilos
   if (overlay.style.display === 'block') {
-    // Si está visible, ocultar el botón
-    btncarrusel.style.display = 'none';
+      // Si está visible, ocultar el botón
+      btncarrusel.style.display = 'none';
   } else {
-    // Si no está visible, mostrar el botón
-    btncarrusel.style.display = 'block';
+      // Si no está visible, mostrar el botón
+      btncarrusel.style.display = 'block';
+      btncarrusel.style.transition = 'none'; // Deshabilitar temporalmente las transiciones    
   }
 }
+
+
   
 
     
